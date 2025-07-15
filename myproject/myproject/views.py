f91e449db3860ba0b9ecff4532ec568bbecd9292
Ashutosh <ashutosh12505@gmail.com>
2025-07-15 19:40:36 +0530

    Initial commit - July 15th, 2025

---
+++ myproject/myproject/views.py
@@ -0,0 +1,9 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Home Page")
+    return render(request, 'home.html')
+def about(request):
+    # return HttpResponse("About")
+    return render(request, 'about.html')
